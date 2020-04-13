dd48d129d9e694517bdda8ac1f7f1b4dcb547cbe
adejeg <adek4life@gmail.com>
2020-04-13 03:29:04 +0100

    Linking to heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/e804bb14208845e252eaeb59ab3f2f0b/' + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, res) => {
+        
+        
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
+        } else if (res.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, res.body.daily.data[0].summary + ' It is currently ' + res.body.currently.temperature + ' degress out. There is a ' + res.body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,16 +3,17 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYWRlamVnIiwiYSI6ImNrN3VmbXVndzAzOWEzbW8yOHI3djU5eHMifQ.QDpwmxlxd2Tx6TWbs4Zz5Q'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, res) => {
+        
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+        } else if (res.body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
-                location: body.features[0].place_name
+                latitude: res.body.features[0].center[1],
+                longitude: res.body.features[0].center[0],
+                location: res.body.features[0].place_name
             })
         }
     })
